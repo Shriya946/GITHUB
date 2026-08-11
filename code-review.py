19accdfe8dbe498eb88b7117fbe7cb7a5b8ec06b
shriya <shriyagautam90@gmail.com>
2026-08-11 22:04:25 +0530

    Added code review example

---
+++ code-review.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+
+print("Hello", name)
+
+password = input("Enter password: ")
+
+if password == "1234":
+    print("Login successful")
+else:
+    print("Wrong password")
